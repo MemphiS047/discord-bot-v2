d998dec6134219088c579415aa14727d111e5fa7
Yigit <46416247+MemphiS047@users.noreply.github.com>
2021-09-15 21:08:28 +0000

    dynamic viewer added with cogs for testing it temporarily

---
+++ app/bot/dynamic_viewer.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+class LolCogs(commands.Cogs):
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @commands.command(name="lolbuild")
+  async def lolbuild_command(self, ctx):
+    channel = ctx.message.channel
+    messages = await channel.history(limit=1).flatten()
+    
